5654c1217698e27065b02cbc722d50677f5f5aaf
Evan Fellman <evan.fellman@gmail.com>
2019-05-21 13:07:40 -0400

    Added ability to make the gif smaller

--- mom.py
+++ mom.py
@@ -17,6 +17,10 @@ def on_move(x, y):
 	pass
 
 
+def floor(x):
+	return round(x - 0.5)
+
+
 def on_click(x, y, button, pressed):
 	global x1
 	global x2
@@ -55,7 +59,6 @@ if y2 < y1:
 	temp = y2
 	y2 = y1
 	y1 = temp
-
 flag = True
 print("\nRecording...\n\nPress control and space on me to finish recording :)")
 images = []
@@ -63,7 +66,8 @@ mousePositions = []
 with mss.mss() as sct:
 	mon = sct.monitors[1]
 monitor = {"top": y1, "left": x1, "width": (x2 - x1), "height": (y2 - y1)}
-speed = 1 / float(open("data/data.txt", "r").read())
+speed = 1 / float(open("data/speed.txt", "r").read())
+makeSmaller = float(open("data/compress.txt", "r").read()) + 2
 while flag:
 	with mss.mss() as sct:
 		mousePositions.append(pyautogui.position())
@@ -73,13 +77,14 @@ while flag:
 			print('Finished recording. Now making the GIF. I will close when it is done.')
 		time.sleep(speed)
 for i in range(len(images)):
+
 	temp = Image.frombytes("RGB", images[i].size, images[i].bgra, "raw", "BGRX")
 	mouseImage = Image.open("data/mouse.png")
 	mx, my = mousePositions[i]
 	mx -= x1
 	my -= y1
 	temp.paste(mouseImage, (mx, my))
-	images[i] = temp
+	images[i] = temp.resize((floor(temp.width / (makeSmaller / 2)), floor(temp.height / (makeSmaller / 2))))
 flag2 = True
 i = 0
 while flag2:
@@ -89,4 +94,4 @@ while flag2:
 			images[0].save('HereYouGo' + str(i) + '.gif', save_all=True, append_images=images[1:], duration=200, loop=0)
 			flag2 = False
 	except Exception:
-		pass
+		print(Exception)
